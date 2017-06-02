c7ce6c51e5077f35dcf703b7d5ddf62b73f142b3
Yura krohmal <yury.krohmal@ardas.dp.ua>
2017-06-02 13:30:34 +0300

    Addede base xcode project with tests

---
+++ ReactiveSwift/ReactiveSwift/ReactiveSwift.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveSwift.h
+//  ReactiveSwift
+//
+//  Created by Yury on 02.06.17.
+//  Copyright © 2017 AllUnite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveSwift.
+FOUNDATION_EXPORT double ReactiveSwiftVersionNumber;
+
+//! Project version string for ReactiveSwift.
+FOUNDATION_EXPORT const unsigned char ReactiveSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveSwift/PublicHeader.h>
+
+
---
+++ ReactiveSwift/ReactiveSwiftTests/ReactiveSwiftTests.swift
@@ -0,0 +1,19 @@
+import Quick
+import Nimble
+
+class ReactiveSwiftTests: QuickSpec {
+    
+    override func spec() {
+        describe("the 'Documentation' directory") {
+            it("has everything you need to get started") {
+                
+            }
+            
+            context("if it doesn't have what you're looking for") {
+                it("needs to be updated") {
+                    
+                }
+            }
+        }
+    }
+}
